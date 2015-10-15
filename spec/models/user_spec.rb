b630ff72691504c4b671d703919b796c81e6c45e
Rodney S <write2rs2@outlook.com>
2015-10-16 09:40:32 +1100

    adds session controller create action

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,4 +15,23 @@ RSpec.describe User, type: :model do
   it { should validate_uniqueness_of(:email) }
   it { should validate_confirmation_of(:password) }
   it { should allow_value('example@domain.com').for(:email) }
+
+  it { should respond_to(:auth_token) }
+
+  it { should validate_uniqueness_of(:auth_token) }
+
+
+  describe "#generate_authentication_toekn!" do
+    it "generates a unique token" do 
+      allow(Devise).to receive_messages(:friendly_token => "uniquetoken123")
+      @user.generate_authentication_token!
+      expect(@user.auth_token).to eq("uniquetoken123")
+    end
+
+    it "generates another token when one already has been taken" do
+      existing_user = FactoryGirl.create(:user, auth_token: "uniquetoken123")
+      @user.generate_authentication_token!
+      expect(@user.auth_token).not_to eq(existing_user.auth_token)
+    end
+  end
 end
